ce91194332438915884cbd243a15f869e30df435
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-25 16:29:14 +0200

    update export

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,4 +1,10 @@
+import axios from 'axios';
+const selectInput = document.querySelector('select.breed-select');
 export function fetchBreeds() {
+  const apiKey =
+    'live_n1sdJMxkaVhtsdFqp4t39Y2DcAMa77V9diAbkhM1vhRFAuJZYL01uyeRM1Hiae7s';
+  axios.defaults.headers.common['x-api-key'] = apiKey;
+  const apiUrl = 'https://api.thecatapi.com/v1/breeds';
   return axios
     .get(apiUrl)
     .then(response => {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import axios from 'axios';
 import { fetchBreeds } from './cat-api';
-const apiUrl = 'https://api.thecatapi.com/v1/breeds';
-const apiKey =
-  'live_n1sdJMxkaVhtsdFqp4t39Y2DcAMa77V9diAbkhM1vhRFAuJZYL01uyeRM1Hiae7s';
-axios.defaults.headers.common['x-api-key'] = apiKey;
 const selectInput = document.querySelector('select.breed-select');
 
 selectInput.addEventListener('click', fetchBreeds);
