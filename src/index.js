3dcccb0853e5a3cb9e5fadb2e602881108457e0a
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-25 18:54:42 +0200

    upup

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import { fetchCatByBreed } from './cat-api';
 const selectInput = document.querySelector('select.breed-select');
 
 selectInput.addEventListener('click', fetchBreeds);
-selectInput.addEventListener('change', fetchCatByBreed());
+selectInput.addEventListener('change', event => {
+  const choosenBreed = event.target.value;
+  fetchCatByBreed(choosenBreed);
+});
