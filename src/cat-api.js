863262f3744648e9e42e58ab1038a2c87dd19e25
JuliaBien <150686648+JuliaBien@users.noreply.github.com>
2024-04-28 12:42:55 +0200

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,7 +3,6 @@ const apiKey =
   'live_n1sdJMxkaVhtsdFqp4t39Y2DcAMa77V9diAbkhM1vhRFAuJZYL01uyeRM1Hiae7s';
 axios.defaults.headers.common['x-api-key'] = apiKey;
 const selectInput = document.querySelector('select.breed-select');
-const catInfoArea = document.querySelector('div.cat-info');
 
 export function fetchBreeds() {
   const apiUrl = 'https://api.thecatapi.com/v1/breeds';
@@ -23,22 +22,16 @@ export function fetchBreeds() {
       console.log(error);
     });
 }
-
 export function fetchCatByBreed(breedId) {
+  const apiUrl = `https://api.thecatapi.com/v1/images/search?${searchParams}`;
   const searchParams = new URLSearchParams({
     breeds_Ids: `${breedId}`,
   });
-  const apiUrl = `https://api.thecatapi.com/v1/images/search?&${searchParams}`;
   return axios
     .get(apiUrl)
     .then(response => {
       console.log(response.data);
-      const breedData = response.data;
-      const catPhoto = document.createElement('img');
-      catPhoto.setAttribute('src', breedData.url);
-      catPhoto.setAttribute('alt', 'cat photo');
-      catPhoto.setAttribute('width', breedData.width);
-      catInfoArea.prepend(catPhoto);
+      return response.data[0];
     })
     .catch(error => {
       console.log(error);
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import axios from 'axios';
 import { fetchBreeds } from './cat-api';
 import { fetchCatByBreed } from './cat-api';
 const selectInput = document.querySelector('select.breed-select');
+const catInfoArea = document.querySelector('div.cat-info');
 
 selectInput.addEventListener('click', fetchBreeds);
 selectInput.addEventListener('change', event => {
-  const choosenBreed = event.target.value;
-  fetchCatByBreed(choosenBreed);
+  const breedId = event.currentTarget.value;
+  fetchCatByBreed(breedId)
+    .then(catInfo => {
+      catInfoArea.innerHTML = `
+    <img src="${catInfo.url}" alt="Cat Image"/>
+    <h2>${catInfo.breeds[0].name}</h2>
+    <p>${catInfo.breeds[0].description}</p>
+    <p>Temperament: ${catInfo.breeds[0].temperament}</p>
+    `;
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
